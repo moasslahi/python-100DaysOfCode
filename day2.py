f77b3da9edb594c4b7107f85c7c760c3e5250278
Mohammed Asslahi <asslahi37@gmail.com>
2019-08-21 14:51:28 +0300

    day 2

---
+++ day2.py
@@ -0,0 +1,16 @@
+#DAY 2, Comments 
+
+# this is a comment 
+print("Hello!, World!")
+
+
+# This is a comment 
+# written in 
+# more than just one line 
+print("Hello!, World!")
+
+
+
+"""
+this is a comment as well !
+"""
