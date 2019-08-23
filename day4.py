3f5b963d95f72e8bf0829d8b87e30cb9b9ff9130
Mohammed Asslahi <asslahi37@gmail.com>
2019-08-24 00:23:11 +0300

    day 4

---
+++ day4.py
@@ -0,0 +1,40 @@
+#Day 4 
+
+#Intiger 
+x = 1 
+y = 3515
+print(type(x))
+print(type(y))
+
+#Float
+a = 1.10
+b = 35.29
+print(type(a))
+print(type(b))
+
+#Complex
+c = 3+5j
+d = 5j
+print(type(c))
+print(type(d))
+
+#Type Conversion 
+t = 1 #int
+f = 2.8 #float 
+g = 1j #complex 
+
+newFloat = float(t)
+newInt = int(f)
+newComplex = complex(g)
+
+print(t)
+print(f)
+print(g)
+
+print(type(t))
+print(type(f))
+print(type(g))
+
+#Random Number
+import random
+print(random.randrange(1,10)) 
