80d929a6746c20632a381db73cf502334b81f05b
Mohammed Asslahi <asslahi37@gmail.com>
2019-08-25 12:17:39 +0300

    Week 2

---
+++ Week2/Day6.py
@@ -0,0 +1,20 @@
+#Python Casting
+# Casting in python is therefore done using constructor functions:
+
+#int()
+x = int(1) 
+y = int(2.8)
+z = int("3")
+print(x,y,z)
+
+#float()
+a = float(1) 
+b = float(2.8)
+c = float("3")
+print(a,b,c)
+
+#str() 
+e = str("s1") 
+f = str(2)
+g = str(3)
+print(e,f,g)
