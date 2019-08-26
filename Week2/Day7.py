6cbc54b9b7fd0fad2e5cf3bb70e3659ce3dbc84f
Mohammed Asslahi <asslahi37@gmail.com>
2019-08-26 23:03:43 +0300

    day7

---
+++ Week2/Day7.py
@@ -0,0 +1,11 @@
+#Day 7 python strings 
+
+print("Hello!")
+
+a = "Hello"
+print(a)
+
+x = "Hello World !"
+print(a[1]) # to get the character at postion 1 
+
+print(a[2:5]) # to get the characters from postion 2 to 5(not included)
