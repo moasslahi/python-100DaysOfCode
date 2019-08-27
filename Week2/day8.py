512e71945915056989be5f9b7d008a0588cbbff4
Mohammed Asslahi <asslahi37@gmail.com>
2019-08-27 14:32:44 +0300

    day 8

---
+++ Week2/day8.py
@@ -0,0 +1,25 @@
+#Day 8 python strings 2 
+
+a = " Hello world ! "
+print(a.strip()) 
+#The strip() method removes any whitespace
+
+a = "My Name is Mo!"
+print(len(a))
+#The len() method returns the length of a string.
+
+a = "HELLO WORLD!"
+print(a.lower())
+#The lower() method returns the string in lower case.
+
+a = "hello world!"
+print(a.upper())
+#The upper() method returns the string in upper case.
+
+a = "python isnt good !"
+print(a.replace("nt", ""))
+#The replace() method replaces a string with another string.
+
+a = "its Mo !"
+print(a.split(","))
+#The split() method splits the string into substrings if it finds instances of the separator.
