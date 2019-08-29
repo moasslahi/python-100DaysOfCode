0a408f91becca9f7b4077f8963de2116ac272832
Mohammed Asslahi <asslahi37@gmail.com>
2019-08-29 15:14:08 +0300

    day10

---
+++ Week2/day10.py
@@ -0,0 +1,22 @@
+# Day 10 python operators
+#Operators are used to perform operations on variables and values.
+x = 10
+y = 5
+#Arithmetic operators 
+print(x + y) #  Addition 
+print(x - y) #  Substraction 
+print(x * y) #  Multiplication
+print(x / y) #  Division 
+print(x % y) #  Modulus 
+print(x ** y)#  Exponentiation 
+print(x // y)#  Floor divisin
+
+#comparison operators 
+print(x == y) # Equal 
+print(x != y) # Not Equal 
+print(x > y)  # Greater than 
+print(x < y)  # Less than 
+print(x >= y) # Greater than or equal to
+print(x <= y) # Less than or equal to 
+
+
