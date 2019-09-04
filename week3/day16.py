363ece11fcf98081d617b59e43e1cd50a98f26cd
Mohammed Asslahi <asslahi37@gmail.com>
2019-09-04 11:58:26 +0300

    day16

---
+++ week3/day16.py
@@ -0,0 +1,16 @@
+#day 16 python Tuples 
+#A tuple is a collection which is ordered and unchangeable.
+#In Python tuples are written with round brackets ().
+
+thistuple = ("apple","banana", "cherry",1 ,2 ,3)
+print(thistuple)
+
+#accessing tuple items
+print(thistuple[1])
+
+#loop through a tuple 
+for x in thistuple:
+    print(x)
+
+#delete a tuple 
+del thistuple
