ba715ecf0ae8afc3e9df823ebfa9d1c25d82e795
Mohammed Asslahi <asslahi37@gmail.com>
2019-09-06 13:46:33 +0300

    Week 3 challenge

---
+++ Challenges/week3Challenge.py
@@ -0,0 +1,18 @@
+# week 3 challenge 
+
+#1, create a list and apply 4 methods
+Mylist = ["Mo",18,"Developer","Football"]
+Mylist.append("Python") #append method adds an element 
+Mylist.insert(1,"Asslahi") #insert method adds an elemnet at the specified element
+Mylist.pop() #pop method removes an element 
+Mylist.reverse() #reverse method reverses the order of the list
+
+print(Mylist)
+
+#2, write a code that checks if the word
+# 'python' is in a tuple
+
+Mytuple = ("Java","Python","Swift")
+
+if "Python" in Mytuple:
+    print("Yes, 'Python' is in the tuple")
