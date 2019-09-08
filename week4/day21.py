9d6b4e3d1510d3d90bbf280bb16552d908d11675
Mohammed Asslahi <asslahi37@gmail.com>
2019-09-08 12:02:18 +0300

    day21

---
+++ week4/day21.py
@@ -0,0 +1,16 @@
+# Day 21 Python Sets 2 
+
+MySet = {"Apple","Cherry","Banana"}
+
+#To determine how many items a set has
+print(len(MySet))
+
+#To remove an item in a set
+MySet.remove("Apple")
+MySet.discard("Apple")
+
+#The clear() method empties the set
+MySet.clear()
+
+#The del keyword will delete the set completely
+del MySet
