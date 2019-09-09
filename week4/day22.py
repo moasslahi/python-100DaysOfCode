760060d5e754c99c13b5a9db3d51c8664b077ca8
Mohammed Asslahi <asslahi37@gmail.com>
2019-09-09 11:50:06 +0300

    day22

---
+++ week4/day22.py
@@ -0,0 +1,29 @@
+#Day 22 python Dictionaries 
+
+'''
+A dictionary is a collection which is unordered,
+changeable and indexed.
+In Python dictionaries are written with curly brackets {}
+, and they have keys and values.
+'''
+
+MyDict = {
+    "Name": "Mo Asslahi",
+    "Hobby": "Football",
+    "FavFood": "Kabsa"
+}
+print(MyDict)
+
+#accessing items
+Myname = MyDict["Name"] #2, Mydict.get("Name")
+print(Myname)
+
+#changing values
+MyDict["FavFood"] = "Pasta"
+
+#looping through a dictionary 
+for x in MyDict:
+    print(MyDict[x])
+
+for x, y in MyDict.items():
+    print(x,y)
