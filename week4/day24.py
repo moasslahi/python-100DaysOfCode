c505c6ae1027f5a3b8ea515b7895a78d1f31d1b4
Mohammed Asslahi <asslahi37@gmail.com>
2019-09-11 10:53:40 +0300

    day24

---
+++ week4/day24.py
@@ -0,0 +1,21 @@
+# Day 24 Python dictionaries 3 
+MyDict = {
+    "Name": "Mo Asslahi",
+    "FavFood": "Kabsa",
+    "Hobby": "Football"
+}
+
+# copy a dictionary
+MyNewDict = MyDict.copy()
+print(MyNewDict)
+
+# nested dictionaries 
+MyFamily = {
+    "child1": {
+        "Name": "Mo"
+    },
+    "Child2": {
+        "Name" : "Ahmed"
+    }
+}
+print(MyFamily)
