6eea6893ebcceebcb1feb36c9063318b72418657
Mohammed Asslahi <asslahi37@gmail.com>
2019-09-16 00:50:04 +0300

    day27

---
+++ week5/day27.py
@@ -0,0 +1,16 @@
+# condiotnal statements 
+a = 33
+b = 200
+
+if b > a:
+    print("b is greater!")
+elif a < b:
+    print("b is greater!")
+else:
+    print("b is greater!")
+
+# shorthand if
+if b > a: print(" b is greater!")
+
+# shorthand if and else
+print("A") if a > b else print("B")
