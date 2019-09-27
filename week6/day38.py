3338e65f35ef58b6989716ada78e63f281d6ad66
Mohammed Asslahi <asslahi37@gmail.com>
2019-09-27 21:38:42 +0300

    day 38

---
+++ week6/day38.py
@@ -0,0 +1,18 @@
+# python arrays 2
+
+
+
+cars = ["Ford","Audi", "Benz"]
+
+#loop through an array
+for x in cars:
+    print(x)
+
+# adding an item
+cars.append("Honda")
+print(cars)
+
+# to remove an item
+cars.pop(0)
+print(cars)
+
