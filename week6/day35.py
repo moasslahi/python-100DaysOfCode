4849ce471e3d06832298859058a4bf980fde0892
Mohammed Asslahi <asslahi37@gmail.com>
2019-09-27 21:27:48 +0300

    day 35

---
+++ week6/day35.py
@@ -0,0 +1,11 @@
+# Python Lambda
+
+#A lambda function is a small anonymous function.
+#A lambda function can take any number of arguments 
+# but can only have one expression.
+
+x = lambda a : a + 10
+print(x(5))
+
+y = lambda a,b : a * b
+print(y(3,4))
